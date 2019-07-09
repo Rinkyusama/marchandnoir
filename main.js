185f6a7d478bb625ad16b2f0fc5420e0d7346c0e
unknown <canteloup.nathan@gmail.com>
2019-07-10 01:11:13 +0200

    mise en place d'un reports

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ client.on('ready', () => {
   console.log(`je suis ${client.user.tag} !`);
 });
 
+client.on('ready', () => {
+  client.user.setActivity("les gens qui font mn?help", {type: "WATCHING"})
+})
 client.on('message', msg => {
   if (msg.content === 'ping') msg.channel.send('Pong!')
 });
@@ -28,6 +31,20 @@ client.on("message", msg => {
    msg.channel.send(embed);
   }
 })
+
+client.on("message", msg => {
+  if(msg.content.startsWith(`${PREFIX}warning`)){
+    var argument = msg.content.split(" ").slice(1);
+    var usertag = msg.mentions.users.first();
+    msg.delete();
+    const embed = new MessageEmbed()
+    .setTitle("ATTENTION")
+    .setColor(990000)
+    .setDescription(`**<@${usertag.id}>**`)
+    .addField("tu as été signalé pour :", `${argument.slice(1).join(" ")}`)
+    msg.channel.send(embed);
+  }
+});
 
 client.on("guildMemberAdd", member => {
   const channel = member.guild.channels.find(ch => ch.name === 'général');
